cd3109e1440f41baf5aa40b8f31d33befe737b69
Dao Le <leanhdaovn@gmail.com>
2019-01-02 23:49:13 +0800

    Add specs

--- spec/firebase_faraday_client_spec.rb
+++ spec/firebase_faraday_client_spec.rb
@@ -1,9 +1,49 @@
+require 'spec_helper'
+
 RSpec.describe FirebaseFaradayClient do
-  it "has a version number" do
-    expect(FirebaseFaradayClient::VERSION).not_to be nil
+  before(:all) { FirebaseFaradayClient.use }
+
+  describe 'Firebase Client' do
+    let(:base_url) { 'https://test.firebaseio.com/' }
+    let(:mock_connection) { double(Faraday::Connection).as_null_object }
+    let(:mock_request) { double.as_null_object }
+    let(:mock_response) { double.as_null_object }
+    let(:params) { { attr: true } }
+
+    before do
+      allow(Faraday).to receive(:new)
+        .with(url: base_url, headers: { 'Content-Type' => 'application/json' })
+        .and_yield(mock_connection)
+        .and_return(mock_connection)
+    end
+
+    let(:client) { Firebase::Client.new(base_url) }
+
+    it 'uses Faraday as http client' do
+      expect(client.request).to eq(client.connection)
+      expect(client.connection).to eq(mock_connection)
+      expect(mock_connection).to have_received(:request).with(:json)
+      expect(mock_connection).to have_received(:response).with(:json)
+      expect(mock_connection).to have_received(:adapter).with(Faraday.default_adapter)
+    end
+
+    it 'processes get requests with faraday' do
+      allow(mock_connection).to receive(:get).and_yield(mock_request).and_return(mock_response)
+      response = client.get('users', params)
+      expect(mock_request).to have_received(:url).with('users.json')
+      expect(mock_request).to have_received(:body=).with(nil)
+      expect(mock_request).to have_received(:params=).with(params)
+      expect(response).to be_a(Firebase::Response)
+      expect(response.response).to eq(mock_response)
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'Firebase Response' do
+    let(:faraday_response) { double(body: { abc: 123 }) }
+    let(:firebase_response) { Firebase::Response.new faraday_response }
+
+    it 'returns reponse body properly' do
+      expect(firebase_response.body).to eq(abc: 123)
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,10 @@
-require "bundler/setup"
-require "firebase_faraday_client"
+require 'bundler/setup'
+require 'firebase'
+require 'firebase_faraday_client'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
