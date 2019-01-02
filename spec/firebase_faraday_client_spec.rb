684f9fe4a98382050bd06f354f2ceadf6991e881
Dao Le <leanhdaovn@gmail.com>
2019-01-03 00:08:31 +0800

    Add specs for all methods

--- spec/firebase_faraday_client_spec.rb
+++ spec/firebase_faraday_client_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe FirebaseFaradayClient do
     let(:mock_request) { double.as_null_object }
     let(:mock_response) { double.as_null_object }
     let(:params) { { attr: true } }
+    let(:data) { { value: 1122 } }
 
     before do
       allow(Faraday).to receive(:new)
@@ -27,14 +28,36 @@ RSpec.describe FirebaseFaradayClient do
       expect(mock_connection).to have_received(:adapter).with(Faraday.default_adapter)
     end
 
-    it 'processes get requests with faraday' do
-      allow(mock_connection).to receive(:get).and_yield(mock_request).and_return(mock_response)
-      response = client.get('users', params)
-      expect(mock_request).to have_received(:url).with('users.json')
-      expect(mock_request).to have_received(:body=).with(nil)
-      expect(mock_request).to have_received(:params=).with(params)
-      expect(response).to be_a(Firebase::Response)
-      expect(response.response).to eq(mock_response)
+    context 'process firebase requests' do
+      before do
+        %i[get put post patch delete].each do |http_method|
+          allow(mock_connection).to receive(http_method)
+            .and_yield(mock_request)
+            .and_return(mock_response)
+        end
+      end
+
+      %i[get delete].each do |http_method|
+        it "processes #{http_method} requests with faraday" do
+          response = client.public_send(http_method, 'users', params)
+          expect(mock_request).to have_received(:url).with('users.json')
+          expect(mock_request).to have_received(:body=).with(nil)
+          expect(mock_request).to have_received(:params=).with(params)
+          expect(response).to be_a(Firebase::Response)
+          expect(response.response).to eq(mock_response)
+        end
+      end
+
+      { set: :put, push: :post, update: :patch }.each do |action, http_method|
+        it "processes #{http_method} requests with faraday" do
+          response = client.public_send(action, 'users', data, params)
+          expect(mock_request).to have_received(:url).with('users.json')
+          expect(mock_request).to have_received(:body=).with(data.to_json)
+          expect(mock_request).to have_received(:params=).with(params)
+          expect(response).to be_a(Firebase::Response)
+          expect(response.response).to eq(mock_response)
+        end
+      end
     end
   end
 
