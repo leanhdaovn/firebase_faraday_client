0c86e79e779b48d5a4829d0960f91e631571853d
Dao Le <leanhdaovn@gmail.com>
2019-01-04 11:36:57 +0800

    Add debugger

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'bundler/setup'
 require 'firebase'
 require 'firebase_faraday_client'
+require 'pry'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
